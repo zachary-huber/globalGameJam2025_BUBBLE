56326960adc4b7f7a57e46c3ba3734ef1447b6da
zachary-huber <zachuber.zh@gmail.com>
2025-01-25 11:18:42 -0600

    added debug ui

---
+++ captain_chair.gd
@@ -0,0 +1,15 @@
+extends Interactable
+
+
+func _init() -> void:
+	interactHintText = "[center]Press [E] to pilot the submarine[/center]"
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+func interact():
+	pass
