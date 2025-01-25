ad05f40a20789a365c9073d99623f2c530d2c3f6
zachary-huber <zachuber.zh@gmail.com>
2025-01-24 22:23:49 -0600

    updated wheel model

--- src/scripts/wheel.gd
+++ src/scripts/wheel.gd
@@ -1,5 +1,6 @@
-extends CollisionShape3D
+extends Node3D
 
+@export var rotationSpeed:float = 2.0
 
 func _ready() -> void:
 	pass
@@ -7,3 +8,9 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	pass
+
+func rotateWheel(direction:String):
+	if direction == "right":
+		$StaticBody3D/wheelSpinPart.rotation.y += rotationSpeed
+	elif direction == "left":
+		$StaticBody3D/wheelSpinPart.rotation.y -= rotationSpeed
